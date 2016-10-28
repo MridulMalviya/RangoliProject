6080b6acb92f9b1bf839818c97f0547ff93ca156
Mridul <mridul.malviya@gmail.com>
2016-10-28 15:09:35 +0530

    update mainfest , gradle add application folder

---
+++ app/src/main/java/com/malviya/rangoliproject/constants/ConstantMessage.java
@@ -0,0 +1,14 @@
+package com.malviya.rangoliproject.constants;
+
+import com.malviya.rangoliproject.R;
+
+/**
+ * Created by Prafulla on 10/28/2016.
+ */
+
+public class ConstantMessage {
+    public static final String SUBJECT = "Happy Diwali";
+    public static final String MESSAGE = "Happy Diwali all of you";
+    public static final String HYPERLINK = "http://malviya.site90.com/";
+    public static final int IMAGE = R.drawable.happy_diwali;
+}
